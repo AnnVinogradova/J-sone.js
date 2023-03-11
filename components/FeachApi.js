41d0c836cea9defeeb254dd016fcd5fdaee51ad1
AnnVinogradova <saikotakaki@gmail.com>
2023-03-11 21:58:43 +0300

    Сортировку не знаю как

--- components/FeachApi.js
+++ components/FeachApi.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-export default function FeachAPI() {
 
+export default function FeachAPI() {
 	const [user, setUser] = useState([])
 
 	const getApi = () => {
@@ -8,10 +8,8 @@ export default function FeachAPI() {
 			.then((response) => response.json())
 			.then((json) => {
 				setUser(json);
-
 			})
 	}
-
 
 	return <>
 		<div className='table-block'>
@@ -20,27 +18,26 @@ export default function FeachAPI() {
 				<table className='table'>
 					<thead>
 						<tr>
-							<th>Name</th>
-							<th>Email</th>
-							<th>Address</th>
-							<th>Phone</th>
-							<th>Website</th>
-							<th>Company.name</th>
+							<th onClick={() => { sortUser(username) }}>Name</th>
+							<th onClick={() => { sortUser(email) }}>Email</th>
+							<th onClick={() => { sortUser(address.city) }}>Address</th>
+							<th onClick={() => { sortUser(phone) }}>Phone</th>
+							<th onClick={() => { sortUser(website) }}>Website</th>
+							<th onClick={() => { sortUser(company.name) }}>Company.name</th>
 						</tr>
 					</thead>
 					<tbody>
-						{user.map((item) =>
-							<tr>
-								<th>{item.name}</th>
+						{user.map((item) => (
+							<tr key={item.id + Math.floor((Math.random() * 999999))}>
+								<th>{item.username}</th>
 								<th>{item.email}</th>
 								<th>{item.address.city}</th>
 								<th>{item.phone}</th>
 								<th>{item.website}</th>
 								<th>{item.company.name}</th>
 							</tr>
-						)}
+						))}
 					</tbody>
-
 				</table>
 			</div>
 		</div>
